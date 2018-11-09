07f5446595211e870e1d97670b4a8e0a2d455d95
Shashank Shet <shashankshet@gmail.com>
2018-11-09 16:29:24 +0530

    Linter ready code

--- sandbox/volume/BM/BM.py
+++ sandbox/volume/BM/BM.py
@@ -1,8 +1,10 @@
-from Process import Process
-from Validate import validate, terminate
 from sys import argv
+from process import Process
+from Validate import validate, terminate, EndGameError, log
 
-ext1, ext2, swap = argv[1:]
+# inSB.py calls BM with bot extensions and a flag
+# if flag==True swap players.
+ext1, ext2, swap = argv[1:4]
 
 try:
     player1 = Process(ext1, "player1")
@@ -17,33 +19,35 @@ except Exception as e:
 if swap == "True":
     (player1, player2) = (player2, player1)
 
-player = (player1, player2)
-actual_and_flipped = [ "", "" ]
+player = [player1, player2]
 
-if player[0].isAlive():
-    player[0].passInput(b'0')
+if player[0].is_alive():
+    player[0].pass_input(b'0')
 else:
-    terminate()
-    
-if player[1].isAlive():
-    player[1].passInput(b'1')
+    terminate(loser=1)
+
+if player[1].is_alive():
+    player[1].pass_input(b'1')
 else:
-    terminate()
+    terminate(loser=2)
 
 try:
     for i in range(50):
-        if player[i%2].isAlive():
-            actual_op = player[i%2].readOutput()
-            if actual_op:
+        if player[i%2].is_alive():
+            actual_op = player[i%2].read_output()
+            if actual_op is not None:
                 flipped_op = validate(actual_op)
-                player[(i+1)%2].passInput(flipped_op)
+                player[(i+1)%2].pass_input(flipped_op)
+                if i%2 == 0:
+                    log(actual_op)
+                else:
+                    log(flipped_op)
             else:
-                terminate()           # TLE
+                terminate(loser=(i%2)+1)     # TLE
         else:
-            terminate()               # Premature Termination.
-    else:
-        terminate()                   # Match did not terminate, DRAW
+            terminate(loser=(i%2)+1)         # Premature Termination.
+    terminate(draw=True)                     # Match did not terminate, DRAW
 except IOError as ioe:
-    pass                              # Unable to read output
+    terminate(loser=(i%2)+1)                 # Unable to read output
 except EndGameError as ege:
-    pass
+    terminate()
---
+++ sandbox/volume/BM/process.py
@@ -0,0 +1,107 @@
+'''
+This module houses the bot process class and signal handler.
+'''
+
+from subprocess import Popen, PIPE, call
+from select import poll, POLLIN
+import signal
+import psutil
+from BMLimits import time_limits, resource_limits, MAX_BUFFER_LENGTH
+
+def handler(signum, frame):
+    '''
+    Custom signal handler for SIGALRM
+    '''
+    raise IOError("Unable to open device")
+
+signal.signal(signal.SIGALRM, handler)
+
+class Process:
+    '''
+    Adapter class to provide a simple API for bot processes, enabling one line
+    initialisation, reading, writing, and polling.
+    '''
+    def __init__(self, ext, exec_name):
+        '''
+        Create a Popen process, save its PID and prlimit it based on ext.
+
+        Parameters
+        ----------
+        ext: str
+            Extension of the bot to be run.
+        exec_name: str
+            Name of the executable which will be run. (player1/player2)
+        '''
+        # Without stdbuf, reading bot output blocks indefinitely.
+        arg_list = ["stdbuf", "-o0", "-e0", "-i0", "../bots/" + exec_name]
+        self.time_limit = time_limits[ext]
+        self.popen_obj = Popen(arg_list, stdin=PIPE, stdout=PIPE)
+        self.set_limits(ext)
+        self.poll_obj = poll()
+        self.proc_obj = psutil.Process(pid=self.popen_obj.pid)
+        self.poll_obj.register(self.popen_obj.stdout.fileno(), POLLIN)
+
+    def set_limits(self, ext):
+        '''
+        Prlimit encapsulated Popen process using limits provided in file
+        BMLimits for bots with extension ext.
+
+        Parameters
+        ----------
+        ext: str
+            Extension of the bot to find limits for.
+        '''
+        prlimit_args = ["prlimit", "-p", str(self.popen_obj.pid)]
+        prlimit_args.extend(resource_limits[ext])
+        call(prlimit_args)
+
+    def suspend(self):
+        '''Suspend one bot to dedicate cpu time to the other'''
+        self.proc_obj.suspend()
+
+    def resume(self):
+        '''Resume current bot'''
+        self.proc_obj.resume()
+
+    def kill(self):
+        '''
+        If process is not dead, kills it.
+        '''
+        if self.popen_obj.poll() is None:
+            self.popen_obj.kill()
+
+    def pass_input(self, ip_string):
+        '''
+        Version independent method of writing input to the processes's stdin
+
+        Parameters
+        ----------
+        ip_string: str
+            String input to be passed to the bot.
+        '''
+        if not isinstance(ip_string, bytes):
+            self.popen_obj.stdin.write(ip_string.encode()+'\n')
+        else:
+            self.popen_obj.stdin.write(ip_string + '\n')
+        self.popen_obj.stdin.flush()
+
+    def read_output(self):
+        '''
+        Read either a line or MAX_BUFFER_LENGTH characters from the processes'
+        stdout and returns it. In case of a timout, it returns None.
+        '''
+        event_list = self.poll_obj.poll(self.time_limit)
+        if event_list and event_list[0][1] is POLLIN:
+            signal.alarm(1)
+            op_string = self.popen_obj.stdout.readline(MAX_BUFFER_LENGTH)
+            signal.alarm(0)
+            return op_string.rstrip('\n')
+        return None
+
+    def is_alive(self):
+        '''
+        Return True is process is alive, False otherwise.
+        '''
+        if self.popen_obj.poll() is None:
+            return True
+        return False
